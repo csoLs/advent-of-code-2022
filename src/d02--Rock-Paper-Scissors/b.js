e92463225f0243fc25a1ac2888c4cfffee32ba54
csoLs <linus.stengard@gmail.com>
2022-12-02 21:08:01 +0100

    🔥 Cleanup d02 part 2

--- src/d02--Rock-Paper-Scissors/b.js
+++ src/d02--Rock-Paper-Scissors/b.js
@@ -1,34 +1,25 @@
 const data = require('./input.js')
 
+const map = new Map([
+  ['A', {name: 'Rock', win: 'Paper', lose: 'Scissors', draw: 'Rock'}],
+  ['B', {name: 'Paper', win: 'Scissors', lose: 'Rock', draw: 'Paper'}],
+  ['C', {name: 'Scissors', win: 'Rock', lose: 'Paper', draw: 'Scissors'}],
+  ['X', {option:'lose',points:0}],
+  ['Y', {option:'draw',points:3}],
+  ['Z', {option:'win',points:6}],
+  ['Rock', 1],
+  ['Paper', 2],
+  ['Scissors', 3]
+])
+
 const parseResult = raw => {
   const input = raw.split('\n').map(game => {
     const [opponent, selection] = game.split(' ')
+    const a = map.get(opponent)
+    const b = map.get(selection)
+    const c = map.get(a[b.option])
 
-    if(opponent === 'A') {
-      if(selection === 'X') { // lose
-        return 3 + 0
-      } else if(selection === 'Y') { // draw
-        return 1 + 3
-      } else {
-        return 2 + 6 // win
-      }
-    } else if(opponent === 'B') {
-      if(selection === 'X') {
-        return 1 + 0
-      } else if(selection === 'Y') {
-        return 2 + 3
-      } else {
-        return 3 + 6
-      }
-    } else {
-      if(selection === 'X') {
-        return 2 + 0
-      } else if(selection === 'Y') {
-        return 3 + 3
-      } else {
-        return 1 + 6
-      }
-    }
+    return b.points + c
   });
 
   return input.reduce((acc,val) => acc+val,0);
